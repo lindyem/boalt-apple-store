4238f3e36fc891690d79c86cdb635a04d4c817d2
lindyem <lindy.merwin@gmail.com>
2020-10-20 17:00:57 -0700

    Welcome Page

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import Reel from 'react-reel'
+import React from 'react';
+import Reel from 'react-reel';
+import appleLogoSmall from '../../assets/images/apple-32.png';
+import appleLogoBig from '../../assets/images/apple-512.png';
+
+const theme = {
+  reel: {
+    height: "1em",
+    display: "flex",
+    alignItems: "flex-end",
+    overflowY: "hidden",
+    fontSize: "25px",
+    fontWeight: "300",
+    color: "#000000",
+    lineHeight: "0.95em"
+  },
+  group: {
+    transitionDelay: "0ms",
+    transitionTimingFunction: "ease-in-out",
+    transform: "translate(0, 0)",
+    height: "1em"
+  },
+  number: {
+    height: "1em"
+  }
+};
+
 
 const Welcome = (props) => {
   return (
     <div className="welcome">
       <div className="welcome__header">
-      
+        <img src={appleLogoSmall}/>
       </div>
       <div className="welcome__body">
-        <img/>
-        <div></div>
-        <Reel text="$34.42" />
+        <div className="welcome__mainContent">
+          <div className="welcome__text">New Products Coming This Summer</div>
+        </div>
+        <Reel text="2019" theme={theme}/>
       </div>
     </div>
   )
